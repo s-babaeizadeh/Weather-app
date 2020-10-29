50a8104f55c17dc7cebe2798cd398a9547dd5f65
SoudehBabaeizadeh <babaeizadeh.s@gmail.com>
2020-10-29 22:58:03 +0330

    Updates

---
+++ precache-manifest.5592269afe84e64768a9431dff78cd98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed933fb0be510790c42f4805c1102bc8",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "a8f117f3ce7fa0c586fc",
+    "url": "/weather-app/static/css/main.06b8ca92.chunk.css"
+  },
+  {
+    "revision": "4a9122be10fdcb24c58d",
+    "url": "/weather-app/static/js/2.a1c9a7a4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weather-app/static/js/2.a1c9a7a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8f117f3ce7fa0c586fc",
+    "url": "/weather-app/static/js/main.00511865.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  },
+  {
+    "revision": "c4c1dd79101e87a5acbdacc5e2307aab",
+    "url": "/weather-app/static/media/b.c4c1dd79.jpg"
+  }
+]);
